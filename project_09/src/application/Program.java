c1c53505511d45dca463aab4cea8e183f53252d9
Tchauperas <marcos.vhynycyus@gmail.com>
2024-08-29 11:05:28 -0300

    commit

---
+++ project_09/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		boolean x = true;
+		
+		while(x) {
+			try {
+				System.out.print("Name: ");
+				String name = sc.nextLine();
+				System.out.print("Age: ");
+				int age = sc.nextInt();
+				System.out.print("Height: ");
+				double height = sc.nextDouble();
+				
+				System.out.println("Name: " + name);
+				System.out.println("Age: " + age);
+				System.out.println("Height: " + height);
+				x = false;
+			} 
+			
+			catch (InputMismatchException e){
+				System.out.println("The age and height fields must be numeric.");
+				sc.nextLine();
+			}
+		}
+		
+		sc.close();
+	}
+}
